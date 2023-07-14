eaa9d3d3284c3c9c728251dbd721c4f8801bfc71
jmanuel <jmanuel@suyati.com>
2023-07-14 11:12:28 +0530

    updated email config

--- src/helper/util/emailUtils/email.ts
+++ src/helper/util/emailUtils/email.ts
@@ -9,7 +9,7 @@ async function sendEmail() {
       secure: false,
       auth: {
         user: 'jeena.manuel@interfacesys.com',
-        pass: 'Interface2',
+        pass: 'Interface3',
       },
     });
 
